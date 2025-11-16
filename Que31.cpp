0db09e98f70aee6bc7c16a96800831f69ff5cad2
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-16 13:43:53 +0530

    String

---
+++ Que31.cpp
@@ -0,0 +1,25 @@
+// 31. Compress the string (like Leetcode 443)
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[]="akashganga";
+    int n=strlen(str); 
+    int freq[256]={0};
+    int i,count;
+    for( i=0;i<n;i++){
+        if(freq[str[i]]==0){
+            count=1;
+            for(int j=i+1;j<n;j++){
+                if(str[i]==str[j]){
+                    count++;
+                }
+            }
+            printf("%c%d",str[i],count);
+            freq[str[i]]=1;//marked visited
+        }
+        
+    
+    }
+    return 0;
+    
+}
